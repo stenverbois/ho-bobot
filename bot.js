6b093801b68a9994e950763bd5aa59621c927a75
Sten Verbois <stenverbois@gmail.com>
2016-06-15 12:00:09 +0200

    Init Heroku app server

--- bot.js
+++ bot.js
@@ -4,4 +4,17 @@ let client = new Client()
 client.on('ready', () => {
     client.setClientGame("WORLD DOMINATION")
 })
+
 client.login("MTkxNTc3NDYwOTI2NTc4Njg5.Cj8T6g.thz8gHSZ1JNNJXHkjJnhgpXXmNM")
+
+// Heroku app setup
+var http = require('http');
+let port = process.env.PORT || 9000
+
+var server = http.createServer((request, response) => {
+    response.end('There are no strings on me!');
+})
+
+server.listen(port, () => {
+    console.log("Server listening on: http://localhost:%s", port);
+})
