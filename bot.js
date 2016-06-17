21d9fde48fa2ee236d12d0087103ec5a082001cb
Sten Verbois <stenverbois@gmail.com>
2016-06-17 18:02:34 +0200

    Cleanup bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,35 @@
-let Client = require('./src/client');
+const http = require('http');
+const Client = require('./src/client');
+
+let web_str = '';
 
 let client = new Client();
 client.on('ready', () => {
-    client.setClientGame("WORLD DOMINATION");
+    client.setClientGame('WORLD DOMINATION');
 });
 
 client.on('server-created', (server) => {
-    if(server.name === "Ho-Bokes") {
+    if(server.name === 'Ho-Bokes') {
         let web_str = 'Members online: ';
         client.guilds.all().forEach(server => {
-            console.log("NAME " + server.name);
+            console.log('NAME ' + server.name);
         });
-        client.guilds.get("name", "Ho-Bokes").members.all("online", true).forEach(member => {
-            web_str += member.username + ", ";
+        client.guilds.get('name', 'Ho-Bokes').members.all('online', true).forEach(member => {
+            web_str += member.username + ', ';
         });
         web_str = web_str.substring(0, web_str.length - 2);
-        // Heroku app page
-        var http = require('http');
-        let port = process.env.PORT || 9000;
-
-        var http_server = http.createServer((request, response) => {
-            response.end(`There are no strings on me!\n\n${web_str}`);
-        });
-
-        http_server.listen(port, () => {
-            console.log("Server listening on: http://localhost:%s", port);
-        });
     }
 });
 
-client.login("MTkxNTc3NDYwOTI2NTc4Njg5.Cj8T6g.thz8gHSZ1JNNJXHkjJnhgpXXmNM");
+client.login('MTkxNTc3NDYwOTI2NTc4Njg5.Cj8T6g.thz8gHSZ1JNNJXHkjJnhgpXXmNM');
+
+// Heroku app page
+const port = process.env.PORT || 9000;
+
+var http_server = http.createServer((request, response) => {
+    response.end(`There are no strings on me!\n\n${web_str}`);
+});
+
+http_server.listen(port, () => {
+    console.log('Server listening on: http://localhost:%s', port);
+});
