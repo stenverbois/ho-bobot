2c31d4d3f4e76978c893f2b885e56210918d769f
tristanvandeputte <tristanvandeputte@hotmail.com>
2016-06-18 16:44:17 +0200

    Forgot files

---
+++ src/emoji.js
@@ -0,0 +1,18 @@
+const Thumbnail = require('./thumbnail');
+const Provider = require('./provider');
+const Collection = require('./collection');
+
+module.exports =
+class Emoji {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.require_colons = data.require_colons;
+        this.managed = data.managed;
+
+        this.roles = Collection();
+        data.roles.forEach(role => {
+            this.roles.add(new Role(role));
+        });
+    }
+};
